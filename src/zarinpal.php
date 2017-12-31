dab324a526efbab22fcea2e6dfa80f1bb467c5f8
Jamal Ghasemi <ghasemi@Lan.Gosafir>
2017-12-31 09:48:23 +0330

    first commit

---
+++ src/zarinpal.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ghasemi
+ * Date: 12/31/2017
+ * Time: 9:13 AM
+ */
+
+namespace dorsaweb;
+
+
+class zarinpal
+{
+    public static function world()
+    {
+        return 'Hello World, Composer!';
+    }
+}
